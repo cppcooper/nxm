b9c091ae89866b9f2c68fc040054e781ff838d58
Josh Cooper <cooper.s.josh@outlook.com>
2021-11-15 16:54:12 -0800

    Adds command parsing structure

---
+++ include/command.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <command-structure.h>
+#include <string>
+#include <cstring>
+#include <cpr/cpr.h>
+
+namespace type {
+    enum command{
+        download,
+        track,
+        untrack,
+        endorse,
+        abstain,
+        list,
+        list_games,
+        list_tracked,
+        list_endorsed,
+        list_trending,
+        list_files,
+        INVALID = 0xFF
+    };
+    enum request{
+        GET,
+        POST,
+        DELETE,
+        NONE = 0xFF
+    };
+}
+
+class Command {
+private:
+    const char* download_uri = "/v1/games/{game_domain_name}/mods/{mod_id}/files/{id}/download_link.json"; //GET
+    const char* track_uri = "/v1/user/tracked_mods.json"; //POST
+    const char* untrack_uri = "/v1/user/tracked_mods.json"; //DELETE
+    const char* endorse_uri = "/v1/games/{game_domain_name}/mods/{id}/endorse.json"; //POST
+    const char* abstain_uri = "/v1/games/{game_domain_name}/mods/{id}/abstain.json"; //POST
+    const char* list_games_uri = "/v1/games.json"; //GET
+    const char* list_tracked_uri = "/v1/user/tracked_mods.json"; //GET
+    const char* list_endorsed_uri = "/v1/user/endorsements.json"; //GET
+    const char* list_trending_uri = "/v1/games/{game_domain_name}/mods/trending.json"; //GET
+    const char* list_files_uri = "/v1/games/{game_domain_name}/mods/{mod_id}/files.json"; //GET
+protected:
+    const Nxm &cli;
+    const CLI::App* command;
+    cpr::Response r;
+    type::command command_type;
+    type::request request_type;
+    std::string uri;
+    void make_uri();
+    void parse_command_type(const std::string &command);
+public:
+    explicit Command(const CLI::App* command, const Nxm &cli) : command(command), cli(cli) {
+        parse_command_type(command->get_name());
+        make_uri();
+    }
+    int run();
+    std::string output() const { return r.text; }
+};
---
+++ src/command.cpp
@@ -0,0 +1,165 @@
+#include <command.h>
+
+namespace globals {
+    extern std::string apikey;
+}
+
+int Command::run(){
+    switch(request_type){
+        case type::GET:
+            r = cpr::Get(cpr::Url(getURI()),
+                         cpr::Header{{"apikey", globals::apikey}});
+            break;
+        case type::POST:
+            switch(command_type){
+                case type::endorse:
+                case type::abstain:
+                    r = cpr::Post(cpr::Url(uri),
+                                  cpr::Header{{"apikey", globals::apikey}});
+                    break;
+                // URI is void of parameters
+                case type::track:
+                    r = cpr::Post(cpr::Url(uri),
+                                  cpr::Header{{"apikey", globals::apikey}},
+                                  cpr::Parameters{{"domain_name", cli.arg1},
+                                                  {"mod_id",      cli.arg2}});
+                    break;
+            }
+            break;
+        case type::DELETE:
+            r = cpr::Delete(cpr::Url(uri),
+                            cpr::Header{{"apikey", globals::apikey}},
+                            cpr::Parameters{{"domain_name", cli.arg1},
+                                            {"mod_id",      cli.arg2}});
+            break;
+        case type::NONE:
+            if(command_type != type::list){
+                return 1;
+            }
+            if (command->get_subcommands().size() != 1) {
+                return 1;
+            }
+            auto sub_command = command->get_subcommands()[0];
+            Command c2(sub_command, cli);
+            return c2.run();
+    }
+    return 0;
+}
+
+void Command::make_uri() {
+    char buffer[256];
+    strcat(buffer,"https://api.nexusmods.com");
+    switch(command_type){
+        case type::INVALID:
+            break;
+        case type::download:
+            strcat(buffer,download_uri);
+            break;
+        case type::track:
+            strcat(buffer,track_uri);
+            break;
+        case type::untrack:
+            strcat(buffer,untrack_uri);
+            break;
+        case type::endorse:
+            strcat(buffer,endorse_uri);
+            break;
+        case type::abstain:
+            strcat(buffer,abstain_uri);
+            break;
+        case type::list:
+        case type::list_games:
+            strcat(buffer,list_games_uri);
+            break;
+        case type::list_tracked:
+            strcat(buffer,list_tracked_uri);
+            break;
+        case type::list_endorsed:
+            strcat(buffer, list_endorsed_uri);
+            break;
+        case type::list_trending:
+            strcat(buffer, list_trending_uri);
+            break;
+        case type::list_files:
+            strcat(buffer, list_files_uri);
+            break;
+    }
+    uri = std::string(buffer);
+    auto replace = [](std::string& str, const std::string& from, const std::string& to) {
+        size_t start_pos = str.find(from);
+        if(start_pos == std::string::npos)
+            return false;
+        str.replace(start_pos, from.length(), to);
+        return true;
+    };
+    switch(command_type){
+        case type::download:
+            replace(uri, "{game_domain_name}", cli.arg1);
+            replace(uri, "{mod_id}", cli.arg2);
+            replace(uri, "{id}", cli.arg3);
+            break;
+        case type::endorse:
+        case type::abstain:
+            replace(uri, "{game_domain_name}", cli.arg1);
+            replace(uri, "{id}", cli.arg2);
+            break;
+        case type::list_trending:
+            replace(uri, "{game_domain_name}", cli.arg1);
+            break;
+        case type::list_files:
+            replace(uri, "{game_domain_name}", cli.arg1);
+            replace(uri, "{mod_id}", cli.arg2);
+            break;
+    }
+}
+
+void Command::parse_command_type(const std::string &command){
+    if(command == "download") {
+        command_type = type::download;
+        request_type = type::GET;
+    }
+    else if(command == "track") {
+        command_type = type::track;
+        request_type = type::POST;
+    }
+    else if(command == "untrack") {
+        command_type = type::untrack;
+        request_type = type::DELETE;
+    }
+    else if(command == "endorse") {
+        command_type = type::endorse;
+        request_type = type::POST;
+    }
+    else if(command == "abstain") {
+        command_type = type::abstain;
+        request_type = type::POST;
+    }
+    else if(command == "list") {
+        command_type = type::list;
+        request_type = type::NONE;
+    }
+    else if(command == "games") {
+        command_type = type::list_games;
+        request_type = type::GET;
+    }
+    else if(command == "tracked") {
+        command_type = type::list_tracked;
+        request_type = type::GET;
+    }
+    else if(command == "endorsed") {
+        command_type = type::list_endorsed;
+        request_type = type::GET;
+    }
+    else if(command == "trending") {
+        command_type = type::list_trending;
+        request_type = type::GET;
+    }
+    else if(command == "mod-files") {
+        command_type = type::list_files;
+        request_type = type::GET;
+    }
+    else {
+        command_type = type::INVALID;
+        request_type = type::NONE;
+    }
+}
