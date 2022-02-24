21b20369186605517df1c613550ff75cbaa3aef5
Josh Cooper <cooper.s.josh@outlook.com>
2022-02-24 12:51:42 -0800

    Adds comments to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -20,49 +20,60 @@ fs::path GetHome() {
 
 
 extern int nxm(std::string &apikey, const Nxm &cli);
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     Nxm cli;
+    // we need to load the config file (has API key inside)
     fs::path home = GetHome();
     fs::path config_path(home.string().append("/.nxm.json"));
-    if(!fs::exists(config_path)){
+    // and if the file doesn't exist we create one and inform the user to populate it with an API key
+    if (!fs::exists(config_path)) {
         std::fstream config_file(config_path, std::fstream::out);
         config_file << "{\n\"apikey\" : \"\"\n}\n";
         config_file.close();
         std::cerr << "No API key configured. Acquire and place an apikey into " << config_path << std::endl;
     } else {
+        // If the file does exist we'll try to load it
         size_t size = fs::file_size(config_path);
         char* buffer = new char[size];
         std::fstream config_file(config_path);
-        config_file.read(buffer,size);
+        config_file.read(buffer, size);
         config_file.close();
-        try {
-            nlm::json config = nlm::json::parse(buffer);
-            auto key = config["apikey"];
-            if (!key.is_null() && key.is_string()) {
-                std::string apikey = key.get<std::string>();
-                CLI11_PARSE(cli.nxm, argc, argv);
-                int status = nxm(apikey, cli);
-                if(status != 0){
-                    switch(status){
-                        case -1:
-                            std::cerr << "Couldn't send request to nexus." << std::endl;
-                            break;
-                        case -2:
-                            std::cerr << "The API key (field: \"apikey\") in ~/.nxm.json is invalid." << std::endl;
-                            break;
-                        case 2:
-                            return 1;
-                        default:
-                            std::cerr << "An unknown error has occurred. Please report this." << std::endl;
+        if (config_file.good()) {
+            try {
+                // Now we parse the file for the API key
+                nlm::json config = nlm::json::parse(buffer);
+                auto key = config["apikey"];
+                // We can't continue if the key doesn't exist, or it doesn't contain a string
+                if (!key.is_null() && key.is_string()) {
+                    std::string apikey = key.get<std::string>();
+                    // Now it makes sense to parse the command line arguments
+                    CLI11_PARSE(cli.nxm, argc, argv);
+                    // Run the program
+                    int status = nxm(apikey, cli);
+                    // Parse return code and print corresponding info
+                    if (status != 0) {
+                        switch (status) {
+                            case -1:
+                                std::cerr << "Couldn't send request to nexus." << std::endl;
+                                break;
+                            case -2:
+                                std::cerr << "The API key (field: \"apikey\") in ~/.nxm.json is invalid." << std::endl;
+                                break;
+                            case 2:
+                                return 1;
+                            default:
+                                std::cerr << "An unknown error has occurred. Please report this." << std::endl;
+                        }
+                        return 1;
                     }
-                    return 1;
+                    return 0;
+                } else {
+                    // If the API key can't be retrieved we advise the user to configure things
+                    std::cerr << "No API key configured. Acquire and place an apikey into " << config_path << std::endl;
                 }
-                return 0;
-            } else {
-                std::cerr << "No API key configured. Acquire and place an apikey into " << config_path << std::endl;
+            } catch (nlm::json::exception e) {
+                std::cerr << e.what();
             }
-        } catch (nlm::json::exception e) {
-            std::cerr << e.what();
         }
     }
     return 1;
